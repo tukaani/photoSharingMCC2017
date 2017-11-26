a283c849e56f92de18f69b347eeec91508cf1db5
Olli Kiljunen <olli.kiljunen@aalto.fi>
2017-11-26 19:33:37 +0200

    fixed GalleryDao.java

--- frontend/PhotoOrganizer/app/src/main/java/com/appspot/mccfall2017g12/photoorganizer/GalleryDao.java
+++ frontend/PhotoOrganizer/app/src/main/java/com/appspot/mccfall2017g12/photoorganizer/GalleryDao.java
@@ -60,15 +60,20 @@ public abstract class GalleryDao {
         if (photo == null)
             return file; // No file is used because the photo does not exist.
 
+        String wasteFile;
+
         if (resolution > photo.resolution.local) {
             updatePhotoFile(photoId, file, resolution);
-            return photo.file; // File changed, the old file is no longer used.
+            wasteFile = photo.file; // File changed, the old file is no longer used.
+        }
+        else {
+            wasteFile = file; // The new file is not used because it doesn't improve resolution.
         }
 
         if (TextUtils.equals(file, photo.file))
-            return null; // The file is the same as the old one. No file should be deleted.
+            return null; // The new file is the same as the old one. No file should be deleted.
 
-        return file; // The new file is not used because it does not improve resolution.
+        return wasteFile;
     }
 
     @Query("SELECT EXISTS(SELECT 1 FROM PhotoSyncLock WHERE photoId=:photoId LIMIT 1)")
@@ -97,11 +102,13 @@ public abstract class GalleryDao {
     @Delete
     public abstract void deletePhotos(Photo... photos);
 
+    @Transaction
     @Query("SELECT * FROM Album NATURAL LEFT OUTER JOIN "
             + "(SELECT albumId, COUNT(*) AS photoCount, MIN(file) AS file "
             + "FROM Photo GROUP BY albumId)")
     public abstract LiveData<Album.Extended[]> loadAllAlbums();
 
+    @Transaction
     @Query("SELECT photoId, author, people, file, albumId, resolution_local, resolution_online, "
             + Photo.Extended.TYPE_ITEM + " AS itemType "
             + "FROM Photo WHERE albumId = :albumId UNION ALL "
@@ -111,6 +118,7 @@ public abstract class GalleryDao {
             + "ORDER BY people, itemType DESC")
     public abstract LiveData<Photo.Extended[]> loadAlbumsPhotosByPeople(String albumId);
 
+    @Transaction
     @Query("SELECT photoId, author, people, file, albumId, resolution_local, resolution_online, "
             + Photo.Extended.TYPE_ITEM + " AS itemType "
             + "FROM Photo WHERE albumId = :albumId UNION ALL "
