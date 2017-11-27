8174450c8bbaf44987f6d349210c2ceda1fa487f
Olli Kiljunen <olli.kiljunen@aalto.fi>
2017-11-27 20:49:59 +0200

    login hotfix

--- frontend/PhotoOrganizer/app/src/main/java/com/appspot/mccfall2017g12/photoorganizer/LoginActivity.java
+++ frontend/PhotoOrganizer/app/src/main/java/com/appspot/mccfall2017g12/photoorganizer/LoginActivity.java
@@ -132,7 +132,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null) startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        //if(currentUser != null) startActivity(new Intent(LoginActivity.this, MainActivity.class));
     }
 
 
