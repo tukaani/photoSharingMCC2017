471195ced512f097b786e4c770c59a5c071b874a
edgar1 <edgar.ruben.pardo@est.fib.upc.edu>
2017-12-01 10:13:12 +0200

    permissions checking

--- frontend/PhotoOrganizer/app/src/main/java/com/appspot/mccfall2017g12/photoorganizer/LoginActivity.java
+++ frontend/PhotoOrganizer/app/src/main/java/com/appspot/mccfall2017g12/photoorganizer/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -54,8 +55,6 @@ public class LoginActivity extends AppCompatActivity {
 
         checkPermissions();
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mStatusTextView = findViewById(R.id.status);
-        mDetailTextView = findViewById(R.id.detail);
         mAuth = FirebaseAuth.getInstance();
         mRegister = (Button) findViewById(R.id.register);
         mRegister.setOnClickListener(new View.OnClickListener() {
@@ -132,6 +131,26 @@ public class LoginActivity extends AppCompatActivity {
                         android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         android.Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE);
             }
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_WRITE_EXTERNAL_STORAGE:
+                if (grantResults.length > 0) {
+                    boolean cameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeExternalFile = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    boolean readExternalFile = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+
+                    if(cameraPermission && readExternalFile & writeExternalFile)
+                    {
+
+                    } else {
+                       checkPermissions();
+                    }
+                }
+                break;
         }
     }
 }
